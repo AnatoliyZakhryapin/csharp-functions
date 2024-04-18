bf69393c1a8d6ef08780fad0aadddee75eaac21e
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-18 14:19:38 +0200

    ADD Functions StasmpaArray and StampaVirgolaESpazio

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            void StampaArray(int[] array)
+            {
+                Console.Write("[");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                        StampaVirgolaESpazio();
+                }
+                Console.Write("]");
+            }
+
+            void StampaVirgolaESpazio()
+            {
+                Console.Write(", ");
+            }
+
         }
+
     }
 }
